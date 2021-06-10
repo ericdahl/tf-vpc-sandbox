adacc0c570bb0caeaf9e282ed2e6e577954b1c04
Eric Dahl <eric.c.dahl@gmail.com>
2021-06-10 14:15:29 -0700

    rename tf resources

--- private-link/vpc_10_1_0_0.tf
+++ private-link/vpc_10_1_0_0.tf
@@ -1,122 +1,125 @@
-resource "aws_vpc" "10_1_0_0" {
+resource "aws_vpc" "vpc_10_1_0_0" {
   cidr_block = "10.1.0.0/16"
 
-  tags {
+  tags = {
     Name = "10.1.0.0/16"
   }
+
+  enable_dns_hostnames = true
+  enable_dns_support = true
 }
 
-resource "aws_subnet" "10_1_0_0_public1" {
-  vpc_id = "${aws_vpc.10_1_0_0.id}"
+resource "aws_subnet" "public1_10_1_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_1_0_0.id}"
 
   cidr_block              = "10.1.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "10_1_0_0_public2" {
-  vpc_id                  = "${aws_vpc.10_1_0_0.id}"
+resource "aws_subnet" "public2_10_1_0_0" {
+  vpc_id                  = "${aws_vpc.vpc_10_1_0_0.id}"
   cidr_block              = "10.1.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "10_1_0_0_public3" {
-  vpc_id                  = "${aws_vpc.10_1_0_0.id}"
+resource "aws_subnet" "public3_10_1_0_0" {
+  vpc_id                  = "${aws_vpc.vpc_10_1_0_0.id}"
   cidr_block              = "10.1.3.0/24"
   availability_zone       = "us-east-1c"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "10_1_0_0_private1" {
-  vpc_id            = "${aws_vpc.10_1_0_0.id}"
+resource "aws_subnet" "private1_10_1_0_0" {
+  vpc_id            = "${aws_vpc.vpc_10_1_0_0.id}"
   cidr_block        = "10.1.101.0/24"
   availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "10_1_0_0_private2" {
-  vpc_id            = "${aws_vpc.10_1_0_0.id}"
+resource "aws_subnet" "private2_10_1_0_0" {
+  vpc_id            = "${aws_vpc.vpc_10_1_0_0.id}"
   cidr_block        = "10.1.102.0/24"
   availability_zone = "us-east-1b"
 }
 
-resource "aws_subnet" "10_1_0_0_private3" {
-  vpc_id            = "${aws_vpc.10_1_0_0.id}"
+resource "aws_subnet" "private3_10_1_0_0" {
+  vpc_id            = "${aws_vpc.vpc_10_1_0_0.id}"
   cidr_block        = "10.1.103.0/24"
   availability_zone = "us-east-1c"
 }
 
-resource "aws_internet_gateway" "10_1_0_0" {
-  vpc_id = "${aws_vpc.10_1_0_0.id}"
+resource "aws_internet_gateway" "gw_10_1_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_1_0_0.id}"
 }
 
-resource "aws_route_table" "10_1_0_0_public" {
-  vpc_id = "${aws_vpc.10_1_0_0.id}"
+resource "aws_route_table" "public_10_1_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_1_0_0.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.10_1_0_0.id}"
+    gateway_id = "${aws_internet_gateway.gw_10_1_0_0.id}"
   }
 }
 
-resource "aws_route_table" "10_1_0_0_private" {
-  vpc_id = "${aws_vpc.10_1_0_0.id}"
+resource "aws_route_table" "private_10_1_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_1_0_0.id}"
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = "${aws_nat_gateway.10_1_0_0_default.id}"
+    nat_gateway_id = "${aws_nat_gateway.default_10_1_0_0.id}"
   }
 }
 
-resource "aws_route_table_association" "10_1_0_0_sub1" {
-  route_table_id = "${aws_route_table.10_1_0_0_public.id}"
-  subnet_id      = "${aws_subnet.10_1_0_0_public1.id}"
+resource "aws_route_table_association" "sub1_10_1_0_0" {
+  route_table_id = "${aws_route_table.public_10_1_0_0.id}"
+  subnet_id      = "${aws_subnet.public1_10_1_0_0.id}"
 }
 
-resource "aws_route_table_association" "10_1_0_0_sub2" {
-  route_table_id = "${aws_route_table.10_1_0_0_public.id}"
-  subnet_id      = "${aws_subnet.10_1_0_0_public2.id}"
+resource "aws_route_table_association" "sub2_10_1_0_0" {
+  route_table_id = "${aws_route_table.public_10_1_0_0.id}"
+  subnet_id      = "${aws_subnet.public2_10_1_0_0.id}"
 }
 
-resource "aws_route_table_association" "10_1_0_0_sub3" {
-  route_table_id = "${aws_route_table.10_1_0_0_public.id}"
-  subnet_id      = "${aws_subnet.10_1_0_0_public3.id}"
+resource "aws_route_table_association" "sub3_10_1_0_0" {
+  route_table_id = "${aws_route_table.public_10_1_0_0.id}"
+  subnet_id      = "${aws_subnet.public3_10_1_0_0.id}"
 }
 
-resource "aws_route_table_association" "10_1_0_0_private_sub1" {
-  route_table_id = "${aws_route_table.10_1_0_0_private.id}"
-  subnet_id      = "${aws_subnet.10_1_0_0_private1.id}"
+resource "aws_route_table_association" "private_sub1_10_1_0_0" {
+  route_table_id = "${aws_route_table.private_10_1_0_0.id}"
+  subnet_id      = "${aws_subnet.private1_10_1_0_0.id}"
 }
 
-resource "aws_route_table_association" "10_1_0_0_private_sub2" {
-  route_table_id = "${aws_route_table.10_1_0_0_private.id}"
-  subnet_id      = "${aws_subnet.10_1_0_0_private2.id}"
+resource "aws_route_table_association" "private_sub2_10_1_0_0" {
+  route_table_id = "${aws_route_table.private_10_1_0_0.id}"
+  subnet_id      = "${aws_subnet.private2_10_1_0_0.id}"
 }
 
-resource "aws_route_table_association" "10_1_0_0_private_sub3" {
-  route_table_id = "${aws_route_table.10_1_0_0_private.id}"
-  subnet_id      = "${aws_subnet.10_1_0_0_private3.id}"
+resource "aws_route_table_association" "private_sub3_10_1_0_0" {
+  route_table_id = "${aws_route_table.private_10_1_0_0.id}"
+  subnet_id      = "${aws_subnet.private3_10_1_0_0.id}"
 }
 
-resource "aws_eip" "10_1_0_0_nat_gateway" {
+resource "aws_eip" "nat_gateway_10_1_0_0" {
   vpc        = true
-  depends_on = ["aws_internet_gateway.10_1_0_0"]
+  depends_on = ["aws_internet_gateway.gw_10_1_0_0"]
 }
 
-resource "aws_nat_gateway" "10_1_0_0_default" {
-  allocation_id = "${aws_eip.10_1_0_0_nat_gateway.id}"
-  subnet_id     = "${aws_subnet.10_1_0_0_public1.id}"
+resource "aws_nat_gateway" "default_10_1_0_0" {
+  allocation_id = "${aws_eip.nat_gateway_10_1_0_0.id}"
+  subnet_id     = "${aws_subnet.public1_10_1_0_0.id}"
 }
 
 #### Security Groups
 
-resource "aws_security_group" "10_1_0_0_allow_egress" {
-  vpc_id = "${aws_vpc.10_1_0_0.id}"
+resource "aws_security_group" "allow_egress_10_1_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_1_0_0.id}"
   name   = "allow_egress"
 }
 
-resource "aws_security_group_rule" "10_1_0_0_allow_egress" {
-  security_group_id = "${aws_security_group.10_1_0_0_allow_egress.id}"
+resource "aws_security_group_rule" "allow_egress_10_1_0_0" {
+  security_group_id = "${aws_security_group.allow_egress_10_1_0_0.id}"
   type              = "egress"
   from_port         = 0
   to_port           = 0
@@ -124,13 +127,13 @@ resource "aws_security_group_rule" "10_1_0_0_allow_egress" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group" "10_1_0_0_allow_22" {
-  vpc_id = "${aws_vpc.10_1_0_0.id}"
+resource "aws_security_group" "allow_22_10_1_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_1_0_0.id}"
   name   = "10_1_0_0_allow_22"
 }
 
-resource "aws_security_group_rule" "10_1_0_0_allow_22" {
-  security_group_id = "${aws_security_group.10_1_0_0_allow_22.id}"
+resource "aws_security_group_rule" "allow_22_10_1_0_0" {
+  security_group_id = "${aws_security_group.allow_22_10_1_0_0.id}"
   type              = "ingress"
   from_port         = 22
   to_port           = 22
@@ -138,30 +141,30 @@ resource "aws_security_group_rule" "10_1_0_0_allow_22" {
   cidr_blocks       = ["${var.admin_ip_cidr}"]
 }
 
-resource "aws_security_group" "10_1_0_0_allow_vpc" {
-  vpc_id = "${aws_vpc.10_1_0_0.id}"
+resource "aws_security_group" "allow_vpc_10_1_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_1_0_0.id}"
   name   = "allow_vpc"
 }
 
-resource "aws_security_group_rule" "10_1_0_0_allow_vpc" {
-  security_group_id = "${aws_security_group.10_1_0_0_allow_vpc.id}"
+resource "aws_security_group_rule" "allow_vpc_10_1_0_0" {
+  security_group_id = "${aws_security_group.allow_vpc_10_1_0_0.id}"
   type              = "ingress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = ["${aws_vpc.10_1_0_0.cidr_block}"]
+  cidr_blocks       = ["${aws_vpc.vpc_10_1_0_0.cidr_block}"]
 }
 
 
-resource "aws_instance" "10_1_0_0_jumphost" {
+resource "aws_instance" "jumphost_10_1_0_0_jumphost" {
   ami           = "${data.aws_ami.freebsd_11.image_id}"
   instance_type = "t2.small"
-  subnet_id     = "${aws_subnet.10_1_0_0_public1.id}"
+  subnet_id     = "${aws_subnet.public1_10_1_0_0.id}"
 
   vpc_security_group_ids = [
-    "${aws_security_group.10_1_0_0_allow_22.id}",
-    "${aws_security_group.10_1_0_0_allow_vpc.id}",
-    "${aws_security_group.10_1_0_0_allow_egress.id}",
+    "${aws_security_group.allow_22_10_1_0_0.id}",
+    "${aws_security_group.allow_vpc_10_1_0_0.id}",
+    "${aws_security_group.allow_egress_10_1_0_0.id}",
   ]
 
   key_name = "${var.key_name}"
@@ -176,7 +179,7 @@ pkg install -y bash
 chsh -s /usr/local/bin/bash ec2-user
 EOF
 
-  tags {
+  tags = {
     Name = "10_1_0_0_jumphost"
   }
 }
@@ -189,9 +192,9 @@ resource "aws_lb" "nlb" {
   internal = "true"
 
   subnets = [
-    "${aws_subnet.10_1_0_0_private1.id}",
-    "${aws_subnet.10_1_0_0_private2.id}",
-    "${aws_subnet.10_1_0_0_private3.id}"
+    "${aws_subnet.private1_10_1_0_0.id}",
+    "${aws_subnet.private2_10_1_0_0.id}",
+    "${aws_subnet.private3_10_1_0_0.id}"
   ]
 }
 
@@ -209,7 +212,7 @@ resource "aws_lb_listener" "nlb" {
 }
 
 resource "aws_lb_target_group" "nlb_tg" {
-  vpc_id = "${aws_vpc.10_1_0_0.id}"
+  vpc_id = "${aws_vpc.vpc_10_1_0_0.id}"
 
   port = 80
   protocol = "TCP"
@@ -217,19 +220,19 @@ resource "aws_lb_target_group" "nlb_tg" {
 
 resource "aws_lb_target_group_attachment" "nlb" {
   target_group_arn = "${aws_lb_target_group.nlb_tg.arn}"
-  target_id = "${aws_instance.10_1_0_0_jumphost.id}"
+  target_id = "${aws_instance.jumphost_10_1_0_0_jumphost.id}"
   port = 80
 }
 
-resource "aws_instance" "10_1_0_0_webserver" {
+resource "aws_instance" "webserver_10_1_0_0" {
   ami           = "${data.aws_ami.freebsd_11.image_id}"
   instance_type = "t2.small"
-  subnet_id     = "${aws_subnet.10_1_0_0_private1.id}"
+  subnet_id     = "${aws_subnet.private1_10_1_0_0.id}"
 
   vpc_security_group_ids = [
-    "${aws_security_group.10_1_0_0_allow_22.id}",
-    "${aws_security_group.10_1_0_0_allow_vpc.id}",
-    "${aws_security_group.10_1_0_0_allow_egress.id}",
+    "${aws_security_group.allow_22_10_1_0_0.id}",
+    "${aws_security_group.allow_vpc_10_1_0_0.id}",
+    "${aws_security_group.allow_egress_10_1_0_0.id}",
   ]
 
   key_name = "${var.key_name}"
@@ -248,19 +251,17 @@ service nginx start
 chsh -s /usr/local/bin/bash ec2-user
 EOF
 
-  tags {
+  tags = {
     Name = "10_1_0_0_webserver"
   }
 }
 
 
-
-
-resource "aws_flow_log" "10_1_0_0" {
+resource "aws_flow_log" "flow_log_10_1_0_0" {
   log_destination      = "${aws_s3_bucket.flow_logs.arn}"
   log_destination_type = "s3"
   traffic_type         = "ALL"
-  vpc_id               = "${aws_vpc.10_1_0_0.id}"
+  vpc_id               = "${aws_vpc.vpc_10_1_0_0.id}"
 }
 
 resource "aws_s3_bucket" "flow_logs" {
--- private-link/vpc_10_2_0_0.tf
+++ private-link/vpc_10_2_0_0.tf
@@ -1,7 +1,7 @@
-resource "aws_vpc" "10_2_0_0" {
+resource "aws_vpc" "vpc_10_2_0_0" {
   cidr_block = "10.2.0.0/16"
 
-  tags {
+  tags = {
     Name = "10.2.0.0/16"
   }
 
@@ -9,117 +9,117 @@ resource "aws_vpc" "10_2_0_0" {
   enable_dns_support = true
 }
 
-resource "aws_subnet" "10_2_0_0_public1" {
-  vpc_id = "${aws_vpc.10_2_0_0.id}"
+resource "aws_subnet" "public1_10_2_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_2_0_0.id}"
 
   cidr_block              = "10.2.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "10_2_0_0_public2" {
-  vpc_id                  = "${aws_vpc.10_2_0_0.id}"
+resource "aws_subnet" "public2_10_2_0_0" {
+  vpc_id                  = "${aws_vpc.vpc_10_2_0_0.id}"
   cidr_block              = "10.2.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "10_2_0_0_public3" {
-  vpc_id                  = "${aws_vpc.10_2_0_0.id}"
+resource "aws_subnet" "public3_10_2_0_0" {
+  vpc_id                  = "${aws_vpc.vpc_10_2_0_0.id}"
   cidr_block              = "10.2.3.0/24"
   availability_zone       = "us-east-1c"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "10_2_0_0_private1" {
-  vpc_id            = "${aws_vpc.10_2_0_0.id}"
+resource "aws_subnet" "private1_10_2_0_0" {
+  vpc_id            = "${aws_vpc.vpc_10_2_0_0.id}"
   cidr_block        = "10.2.101.0/24"
   availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "10_2_0_0_private2" {
-  vpc_id            = "${aws_vpc.10_2_0_0.id}"
+resource "aws_subnet" "private2_10_2_0_0" {
+  vpc_id            = "${aws_vpc.vpc_10_2_0_0.id}"
   cidr_block        = "10.2.102.0/24"
   availability_zone = "us-east-1b"
 }
 
-resource "aws_subnet" "10_2_0_0_private3" {
-  vpc_id            = "${aws_vpc.10_2_0_0.id}"
+resource "aws_subnet" "private3_10_2_0_0" {
+  vpc_id            = "${aws_vpc.vpc_10_2_0_0.id}"
   cidr_block        = "10.2.103.0/24"
   availability_zone = "us-east-1c"
 }
 
-resource "aws_internet_gateway" "10_2_0_0" {
-  vpc_id = "${aws_vpc.10_2_0_0.id}"
+resource "aws_internet_gateway" "gw_10_2_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_2_0_0.id}"
 }
 
-resource "aws_route_table" "10_2_0_0_public" {
-  vpc_id = "${aws_vpc.10_2_0_0.id}"
+resource "aws_route_table" "public_10_2_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_2_0_0.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.10_2_0_0.id}"
+    gateway_id = "${aws_internet_gateway.gw_10_2_0_0.id}"
   }
 }
 
-resource "aws_route_table" "10_2_0_0_private" {
-  vpc_id = "${aws_vpc.10_2_0_0.id}"
+resource "aws_route_table" "private_10_2_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_2_0_0.id}"
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = "${aws_nat_gateway.10_2_0_0_default.id}"
+    nat_gateway_id = "${aws_nat_gateway.default_10_2_0_0.id}"
   }
 }
 
-resource "aws_route_table_association" "10_2_0_0_sub1" {
-  route_table_id = "${aws_route_table.10_2_0_0_public.id}"
-  subnet_id      = "${aws_subnet.10_2_0_0_public1.id}"
+resource "aws_route_table_association" "sub1_10_2_0_0" {
+  route_table_id = "${aws_route_table.public_10_2_0_0.id}"
+  subnet_id      = "${aws_subnet.public1_10_2_0_0.id}"
 }
 
-resource "aws_route_table_association" "10_2_0_0_sub2" {
-  route_table_id = "${aws_route_table.10_2_0_0_public.id}"
-  subnet_id      = "${aws_subnet.10_2_0_0_public2.id}"
+resource "aws_route_table_association" "sub2_10_2_0_0" {
+  route_table_id = "${aws_route_table.public_10_2_0_0.id}"
+  subnet_id      = "${aws_subnet.public2_10_2_0_0.id}"
 }
 
-resource "aws_route_table_association" "10_2_0_0_sub3" {
-  route_table_id = "${aws_route_table.10_2_0_0_public.id}"
-  subnet_id      = "${aws_subnet.10_2_0_0_public3.id}"
+resource "aws_route_table_association" "sub3_10_2_0_0" {
+  route_table_id = "${aws_route_table.public_10_2_0_0.id}"
+  subnet_id      = "${aws_subnet.public3_10_2_0_0.id}"
 }
 
-resource "aws_route_table_association" "10_2_0_0_private_sub1" {
-  route_table_id = "${aws_route_table.10_2_0_0_private.id}"
-  subnet_id      = "${aws_subnet.10_2_0_0_private1.id}"
+resource "aws_route_table_association" "private_sub1_10_2_0_0" {
+  route_table_id = "${aws_route_table.private_10_2_0_0.id}"
+  subnet_id      = "${aws_subnet.private1_10_2_0_0.id}"
 }
 
-resource "aws_route_table_association" "10_2_0_0_private_sub2" {
-  route_table_id = "${aws_route_table.10_2_0_0_private.id}"
-  subnet_id      = "${aws_subnet.10_2_0_0_private2.id}"
+resource "aws_route_table_association" "private_sub2_10_2_0_0" {
+  route_table_id = "${aws_route_table.private_10_2_0_0.id}"
+  subnet_id      = "${aws_subnet.private2_10_2_0_0.id}"
 }
 
-resource "aws_route_table_association" "10_2_0_0_private_sub3" {
-  route_table_id = "${aws_route_table.10_2_0_0_private.id}"
-  subnet_id      = "${aws_subnet.10_2_0_0_private3.id}"
+resource "aws_route_table_association" "private_sub3_10_2_0_0" {
+  route_table_id = "${aws_route_table.private_10_2_0_0.id}"
+  subnet_id      = "${aws_subnet.private3_10_2_0_0.id}"
 }
 
-resource "aws_eip" "10_2_0_0_nat_gateway" {
+resource "aws_eip" "nat_gateway_10_2_0_0" {
   vpc        = true
-  depends_on = ["aws_internet_gateway.10_2_0_0"]
+  depends_on = ["aws_internet_gateway.gw_10_2_0_0"]
 }
 
-resource "aws_nat_gateway" "10_2_0_0_default" {
-  allocation_id = "${aws_eip.10_2_0_0_nat_gateway.id}"
-  subnet_id     = "${aws_subnet.10_2_0_0_public1.id}"
+resource "aws_nat_gateway" "default_10_2_0_0" {
+  allocation_id = "${aws_eip.nat_gateway_10_2_0_0.id}"
+  subnet_id     = "${aws_subnet.public1_10_2_0_0.id}"
 }
 
 #### Security Groups
 
-resource "aws_security_group" "10_2_0_0_allow_egress" {
-  vpc_id = "${aws_vpc.10_2_0_0.id}"
+resource "aws_security_group" "allow_egress_10_2_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_2_0_0.id}"
   name   = "allow_egress"
 }
 
-resource "aws_security_group_rule" "10_2_0_0_allow_egress" {
-  security_group_id = "${aws_security_group.10_2_0_0_allow_egress.id}"
+resource "aws_security_group_rule" "allow_egress_10_2_0_0" {
+  security_group_id = "${aws_security_group.allow_egress_10_2_0_0.id}"
   type              = "egress"
   from_port         = 0
   to_port           = 0
@@ -127,13 +127,13 @@ resource "aws_security_group_rule" "10_2_0_0_allow_egress" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group" "10_2_0_0_allow_22" {
-  vpc_id = "${aws_vpc.10_2_0_0.id}"
+resource "aws_security_group" "allow_22_10_2_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_2_0_0.id}"
   name   = "10_2_0_0_allow_22"
 }
 
-resource "aws_security_group_rule" "10_2_0_0_allow_22" {
-  security_group_id = "${aws_security_group.10_2_0_0_allow_22.id}"
+resource "aws_security_group_rule" "allow_22_10_2_0_0" {
+  security_group_id = "${aws_security_group.allow_22_10_2_0_0.id}"
   type              = "ingress"
   from_port         = 22
   to_port           = 22
@@ -141,31 +141,30 @@ resource "aws_security_group_rule" "10_2_0_0_allow_22" {
   cidr_blocks       = ["${var.admin_ip_cidr}"]
 }
 
-resource "aws_security_group" "10_2_0_0_allow_vpc" {
-  vpc_id = "${aws_vpc.10_2_0_0.id}"
+resource "aws_security_group" "allow_vpc_10_2_0_0" {
+  vpc_id = "${aws_vpc.vpc_10_2_0_0.id}"
   name   = "allow_vpc"
 }
 
-resource "aws_security_group_rule" "10_2_0_0_allow_vpc" {
-  security_group_id = "${aws_security_group.10_2_0_0_allow_vpc.id}"
+resource "aws_security_group_rule" "allow_vpc_10_2_0_0" {
+  security_group_id = "${aws_security_group.allow_vpc_10_2_0_0.id}"
   type              = "ingress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = ["${aws_vpc.10_2_0_0.cidr_block}"]
+  cidr_blocks       = ["${aws_vpc.vpc_10_2_0_0.cidr_block}"]
 }
 
 
-
-resource "aws_instance" "10_2_0_0_jumphost" {
+resource "aws_instance" "jumphost_10_2_0_0_jumphost" {
   ami           = "${data.aws_ami.freebsd_11.image_id}"
   instance_type = "t2.small"
-  subnet_id     = "${aws_subnet.10_2_0_0_public1.id}"
+  subnet_id     = "${aws_subnet.public1_10_2_0_0.id}"
 
   vpc_security_group_ids = [
-    "${aws_security_group.10_2_0_0_allow_22.id}",
-    "${aws_security_group.10_2_0_0_allow_vpc.id}",
-    "${aws_security_group.10_2_0_0_allow_egress.id}",
+    "${aws_security_group.allow_22_10_2_0_0.id}",
+    "${aws_security_group.allow_vpc_10_2_0_0.id}",
+    "${aws_security_group.allow_egress_10_2_0_0.id}",
   ]
 
   key_name = "${var.key_name}"
@@ -180,26 +179,26 @@ pkg install -y bash
 chsh -s /usr/local/bin/bash ec2-user
 EOF
 
-  tags {
+  tags = {
     Name = "10_2_0_0_jumphost"
   }
 }
 
 
 resource "aws_vpc_endpoint" "nlb" {
-  vpc_id            = "${aws_vpc.10_2_0_0.id}"
+  vpc_id            = "${aws_vpc.vpc_10_2_0_0.id}"
   service_name      = "${aws_vpc_endpoint_service.nlb.service_name}"
   vpc_endpoint_type = "Interface"
 
   security_group_ids = [
-    "${aws_security_group.10_2_0_0_allow_egress.id}",
-    "${aws_security_group.10_2_0_0_allow_vpc.id}",
+    "${aws_security_group.allow_egress_10_2_0_0.id}",
+    "${aws_security_group.allow_vpc_10_2_0_0.id}",
   ]
 
   subnet_ids = [
-    "${aws_subnet.10_2_0_0_private1.id}",
-    "${aws_subnet.10_2_0_0_private2.id}",
-    "${aws_subnet.10_2_0_0_private3.id}",
+    "${aws_subnet.private1_10_2_0_0.id}",
+    "${aws_subnet.private2_10_2_0_0.id}",
+    "${aws_subnet.private3_10_2_0_0.id}",
   ]
 
 }
