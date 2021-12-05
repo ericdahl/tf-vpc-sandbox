f9bf3f15d7e21f67aff0505e7623fb7cda820af0
Eric Dahl <eric.c.dahl@gmail.com>
2021-12-05 12:13:43 -0800

    disable default association

--- transit-gateway-centralized-east-west-net-fw/tgw.tf
+++ transit-gateway-centralized-east-west-net-fw/tgw.tf
@@ -1,6 +1,6 @@
 resource "aws_ec2_transit_gateway" "default" {
-  default_route_table_association = "enable" # TODO: false?
-  default_route_table_propagation = "enable"
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
 
   tags = {
     Name = "transit-gateway-centralized-east-west-fw"
