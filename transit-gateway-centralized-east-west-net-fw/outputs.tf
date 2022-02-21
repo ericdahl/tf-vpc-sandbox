ae4b72a4a85b43ad1373c06b41634d47670eac4c
Eric Dahl <eric.c.dahl@gmail.com>
2022-02-21 12:38:09 -0800

    cleanup outputs

--- transit-gateway-centralized-east-west-net-fw/outputs.tf
+++ transit-gateway-centralized-east-west-net-fw/outputs.tf
@@ -1,38 +1,8 @@
-output "ip" {
+output "vpc" {
   value = {
-    dev = {
-      vpc_10_1_0_0 = {
-        jumphost = {
-          public_ip  = module.vpc_10_1_0_0.ec2_jumphost.public_ip
-          private_ip = module.vpc_10_1_0_0.ec2_jumphost.private_ip
-        }
-        internal = {
-          public_ip  = module.vpc_10_1_0_0.ec2_internal.public_ip
-          private_ip = module.vpc_10_1_0_0.ec2_internal.private_ip
-        }
-      }
-      vpc_10_2_0_0 = {
-        jumphost = {
-          public_ip  = module.vpc_10_2_0_0.ec2_jumphost.public_ip
-          private_ip = module.vpc_10_2_0_0.ec2_jumphost.private_ip
-        }
-      }
-    }
-    stage = {
-      vpc_10_10_0_0 = {
-        jumphost = {
-          public_ip  = module.vpc_10_10_0_0.ec2_jumphost.public_ip
-          private_ip = module.vpc_10_10_0_0.ec2_jumphost.private_ip
-        }
-      }
-    }
-    central = {
-      vpc_10_111_0_0 = {
-        jumphost = {
-          public_ip  = aws_instance.r10_111_0_0_jumphost.public_ip
-          private_ip = aws_instance.r10_111_0_0_jumphost.private_ip
-        }
-      }
-    }
+    vpc_10_1_0_0 = module.vpc_10_1_0_0
+    vpc_10_2_0_0 = module.vpc_10_2_0_0
+    vpc_10_3_0_0 = module.vpc_10_3_0_0
+    vpc_10_3_0_0 = module.vpc_10_10_0_0
   }
 }
