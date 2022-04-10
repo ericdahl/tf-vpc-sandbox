5a2382e52adeb7c152d21443727888d5debf243c
Eric Dahl <eric.c.dahl@gmail.com>
2022-04-10 12:11:29 -0700

    fw - cleanup example rules

--- transit-gateway-centralized-east-west-net-fw/fw_rules_stateful_low_priority.tf
+++ transit-gateway-centralized-east-west-net-fw/fw_rules_stateful_low_priority.tf
@@ -25,12 +25,22 @@ resource "aws_networkfirewall_rule_group" "stateful_strict_low_priority" {
 
     rules_string = <<EOF
 
+pass icmp $RFC_1918 any -> any any (msg: "allow all rfc1918 icmp"; sid: 1;)
 
-pass http $RFC_1918 any -> $EXTERNAL_NET 80 (http.host; dotprefix; content:".example.com"; endswith; msg:"Allowed HTTP domain"; sid:892120; rev:1;)
-pass tcp $RFC_1918 any <> $EXTERNAL_NET 80 (flow:not_established; sid:892191; rev:1;)
+# example - drop requests from "bad_user" HTTP User Agent
+drop http $RFC_1918 any -> any any (content: "bad_user"; http_user_agent; sid: 2; rev: 1;)
+
+# allow http to example.com domain
+pass http $RFC_1918 any -> !$RFC_1918 80 (http.host; dotprefix; content:".example.com"; endswith; msg:"Allowed HTTP domain"; sid:3; rev:1;)
+
+# allow http/tls to .amazon and example.com
+pass tls $RFC_1918 any -> !$RFC_1918 any (tls.sni; dotprefix; content:".amazon.com"; nocase; endswith; msg:"matching TLS allowlisted FQDNs"; flow:to_server, established; sid:4; rev:1;)
+pass tls $RFC_1918 any -> !$RFC_1918 any (tls.sni; content:"httpbin.org"; startswith; nocase; endswith; msg:"matching TLS allowlisted FQDNs"; flow:to_server, established; sid:5; rev:1;)
 
 
-alert http any any -> any 80 (msg: "alert all http port 80"; sid: 1234;)
+# log anything that did not pass/drop yet
+#alert http any any -> any 80 (msg: "alert all http port 80"; sid: 7;)
+#alert tls any any -> any 443 (msg: "alert all https port 443"; sid: 8;)
 
 EOF
 
