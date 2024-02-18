81195a107f019a0e85b2c0db277c507538340afc
Eric Dahl <eric.c.dahl@gmail.com>
2024-02-18 10:38:08 -0800

    misc rule

--- modules/net-fw/fw_rules_stateful_high_priority.tf
+++ modules/net-fw/fw_rules_stateful_high_priority.tf
@@ -42,9 +42,12 @@ pass tcp any any -> any 80 (flow:established,to_server;sid: 2;)
 # TLS SNI test - block access to httpbin. Note: important that 2nd rule has flow qualifier - otherwise won't work
 # if omitting flow keyword then 2nd rule will allow the connection for the initial unestablished packets, prior
 # to L7 TLS inspection. 2nd rule can be ip/tcp/tls
-drop tls 10.0.0.0/8 any -> any any (tls.sni; content:"httpbin.org"; startswith; nocase; msg:"block access to httpbin"; flow:to_server; sid:3;)
-pass tcp 10.0.0.0/8 any -> !10.0.0.0/8 443 (flow:established,to_server; msg: "default allow egress to internet on port 443"; sid: 4;)
+#drop tls 10.0.0.0/8 any -> any any (tls.sni; content:"httpbin.org"; startswith; nocase; msg:"block access to httpbin"; flow:to_server; sid:3;)
+#pass tcp 10.0.0.0/8 any -> !10.0.0.0/8 443 (flow:established,to_server; msg: "default allow egress to internet on port 443"; sid: 4;)
 
+
+drop tls 10.0.0.0/8 any -> any any (tls.sni; content:"httpbin.org"; startswith; nocase; msg:"block access to httpbin"; flow:to_server; sid:3;)
+pass ip 10.0.0.0/8 any -> !10.0.0.0/8 any (flow:established, to_server; msg: "default allow egress to internet"; sid: 1000001;)
 
 EOF
 
