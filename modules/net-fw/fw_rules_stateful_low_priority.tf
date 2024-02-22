1ef189dd0e1afdc2ae0b2ba5dad849530a268dda
Eric Dahl <eric.c.dahl@gmail.com>
2024-02-22 11:51:00 -0800

    update

--- modules/net-fw/fw_rules_stateful_low_priority.tf
+++ modules/net-fw/fw_rules_stateful_low_priority.tf
@@ -25,6 +25,11 @@ resource "aws_networkfirewall_rule_group" "stateful_strict_low_priority" {
 
     rules_string = <<EOF
 
+# copied from AWS managed rules for Exploits
+# Test case:
+#  curl 'http://example.com/admin/config.php?password\[0\]username=foo'
+drop http any any -> any any (msg:"FreePBX Authentication Bypass (CVE-2019-19006)";flow:established,to_server;http.uri;content:"/admin/config.php?password[0]";content:"username=";distance:0;reference:cve,2019-19006;sid:2846418;rev:1;metadata:affected_product FreePBX,attack_target Web_Server,created_at 2021_01_08,cve CVE_2019_19006,deployment Perimeter,signature_severity Minor;)
+
 #pass ip $RFC_1918 any -> !$RFC_1918 any (msg: "default allow egress to internet"; sid: 1000001;)
 pass ip $RFC_1918 any -> !$RFC_1918 any (flow:established,to_server; msg: "default allow egress to internet"; sid: 1000001;)
 
