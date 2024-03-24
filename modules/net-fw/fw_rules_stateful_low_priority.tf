602ac0412e92dc6a91e98a0b19967b4aded9b571
Eric Dahl <eric.c.dahl@gmail.com>
2024-03-24 11:21:46 -0700

    add fw tests

--- modules/net-fw/fw_rules_stateful_low_priority.tf
+++ modules/net-fw/fw_rules_stateful_low_priority.tf
@@ -28,10 +28,20 @@ resource "aws_networkfirewall_rule_group" "stateful_strict_low_priority" {
 # copied from AWS managed rules for Exploits
 # Test case:
 #  curl 'http://example.com/admin/config.php?password\[0\]username=foo'
-drop http any any -> any any (msg:"FreePBX Authentication Bypass (CVE-2019-19006)";flow:established,to_server;http.uri;content:"/admin/config.php?password[0]";content:"username=";distance:0;reference:cve,2019-19006;sid:2846418;rev:1;metadata:affected_product FreePBX,attack_target Web_Server,created_at 2021_01_08,cve CVE_2019_19006,deployment Perimeter,signature_severity Minor;)
+#drop http any any -> any any (msg:"FreePBX Authentication Bypass (CVE-2019-19006)";flow:established,to_server;http.uri;content:"/admin/config.php?password[0]";content:"username=";distance:0;reference:cve,2019-19006;sid:2846418;rev:1;metadata:affected_product FreePBX,attack_target Web_Server,created_at 2021_01_08,cve CVE_2019_19006,deployment Perimeter,signature_severity Minor;)
+
+drop http $RFC_1918 any -> !$RFC_1918 any (flow:established,to_server; content:"foobar"; sid:11111;)
+
+# response HTML says "Example Domain" here. This will block traffic when making outgoing
+# requests from the VPCs to Internet if response has this string
+drop http !$RFC_1918 any -> $RFC_1918 any (flow:established; content:"Example"; sid:11112;)
+
+# doesn't drop packets - data flow is not to_server for this case
+#drop http !$RFC_1918 any -> $RFC_1918 any (flow:established, to_server; content:"Example"; sid:11112;)
+
 
 #pass ip $RFC_1918 any -> !$RFC_1918 any (msg: "default allow egress to internet"; sid: 1000001;)
-pass ip $RFC_1918 any -> !$RFC_1918 any (flow:established,to_server; msg: "default allow egress to internet"; sid: 1000001;)
+pass ip $RFC_1918 any -> !$RFC_1918 any (flow:established; msg: "default allow egress to internet"; sid: 1000001;)
 
 
 EOF
